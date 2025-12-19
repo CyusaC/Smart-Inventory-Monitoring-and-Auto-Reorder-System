8c58f9545832a731730f519199a8f02aed3fd5cf
cyusa <167714561+CyusaC@users.noreply.github.com>
2025-12-19 11:44:27 +0200

    Modify insert statements for products and inventory
    Updated SQL insert statements for products and inventory to include reorder level and adjusted quantity.

--- database/scripts/03_insert_data.sql
+++ database/scripts/03_insert_data.sql
@@ -1,10 +1,15 @@
+
+
 INSERT INTO suppliers (supplier_name, email, phone)
 VALUES ('ABC Suppliers', 'abc@supply.com', '0788000000');
 
-INSERT INTO products (product_name, supplier_id, unit_price)
-VALUES ('Laptop', 1, 1200);
+INSERT INTO products (product_name, unit_price, reorder_level, supplier_id)
+VALUES ('Laptop', 1200, 10, 1);
 
-INSERT INTO inventory (product_id, quantity, reorder_level)
-VALUES (1, 10, 5);
+INSERT INTO inventory (product_id, quantity)
+VALUES (1, 5);
+
+INSERT INTO holidays (holiday_date, description)
+VALUES (TRUNC(SYSDATE), 'Test Public Holiday');
 
 COMMIT;
