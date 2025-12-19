f0018470c743dac71cc901db652e55d696a6c81d
cyusa <167714561+CyusaC@users.noreply.github.com>
2025-12-19 09:40:23 +0200

    Update 01_create_user.sql

--- database/scripts/01_create_user.sql
+++ database/scripts/01_create_user.sql
@@ -1,3 +1,6 @@
+-- =====================================
+-- 01_create_user.sql
+-- =====================================
 
 CREATE USER smart_inventory
 IDENTIFIED BY smartinventory
@@ -5,6 +8,5 @@ DEFAULT TABLESPACE users
 TEMPORARY TABLESPACE temp
 QUOTA UNLIMITED ON users;
 
--- Grant privileges
 GRANT CONNECT, RESOURCE TO smart_inventory;
 GRANT CREATE VIEW, CREATE SEQUENCE, CREATE PROCEDURE, CREATE TRIGGER TO smart_inventory;
